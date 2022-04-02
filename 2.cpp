2df4c21e342d1919fbdeac6977f9067023dab728
szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-02 19:23:30 +0200

    done

---
+++ 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // vector<int> v1;
+
+    // for (int i = 0; i < 17; i++)
+    //     v1.push_back(i);
+
+    // for (auto i = v1.begin(); i != v1.end(); ++i)
+    //     cout << *i << " ";
+
+    string txt;
+
+    getline(cin, txt, 'n');
+    cout << txt << endl;
+
+
+    return 0;
+}
