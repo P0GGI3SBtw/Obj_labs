f7f90433e8c36fcc7644a2c3e9bb9ddad2a704bf
Szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-09 19:45:37 +0200

    done

---
+++ car.cpp
@@ -0,0 +1,42 @@
+#include "car.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Car::wczytaj()
+{
+    cout << "Podaj marke samochodu: " << endl;
+    cin >> marka;
+
+    cout << "Podaj model samochodu: " << endl;
+    cin >> model;
+
+    cout << "Podaj rocznik samochodu: " << endl;
+    cin >> rocznik;
+
+    cout << "Podaj przebieg samochodu: " << endl;
+    cin >> przebieg;
+}
+
+void Car::wypisz()
+{
+    cout << "Marka samochodu: " << marka << endl;
+    cout << "Model samochodu: " << model << endl;
+    cout << "Rocznik samochodu: " << rocznik << endl;
+    cout << "Przebieg samochodu: " << przebieg << endl;
+}
+
+Car::Car(string m, string md, int r, int p)
+{
+    marka = m;
+    model = md;
+    rocznik = r;
+    przebieg = p;
+}
+
+Car::~Car()
+{
+    cout << "It's me destructor!" << endl;
+}
