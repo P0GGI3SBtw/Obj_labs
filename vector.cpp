359dad0dd3627244dd2bc071268c27854f1fb298
Szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-09 13:16:15 +0200

    done

--- vector.cpp
+++ vector.cpp
@@ -7,16 +7,14 @@ int main()
 {
     vector <int> v1;
 
-    // Dodawanie elementów do wektora
-    for (int i = 1; i <= 18; i++)
+    for (int i = 0; i < 18; i++)
     {
         v1.push_back(i);
     }
 
-    // Wyświetlanie wektora
-    for (auto i = v1.begin(); i != v1.end(); i++)
+    for (int i: v1)
     {
-        cout << *i << " ";
+        cout << i << ", ";
     }
 
     return 0;
