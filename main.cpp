6f8db248b2bd249648c3f0899105ebe31f35e201
Szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-18 21:14:33 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "Ekran.h"
 #include "Ekran.cpp"
 #include "Ramka.h"
@@ -10,12 +11,18 @@ int main()
 {
     Ekran e;
     e.wypelnij('.');
-    /*
-    e.rysuj_text(52,21,"Zosia");
-    e.wyswietl();
-    */
-    Ramka r(2, 3, 30, 10, "Spis Tresci");
-    r.rysuj(e);
+
+    Ramka r1(2, 3, 30, 10, "Spis Tresci");
+    r1.rysuj(e);
+
+    Ramka r2(2, 15, 30, 10, "Rozdzial 1");
+    r2.rysuj(e);
+
+    Ramka r3(2, 27, 30, 10, "Rozdzial 2");
+    r3.rysuj(e);
+
+    e.rysuj_text(1,1,"Zosia");
+
     e.wyswietl();
 
     return 0;
